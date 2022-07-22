e74d1b93f5b87513c1e2a8eafbb148b6c5b6af7a
Anthony Yos <anthonyvyos@gmail.com>
2022-07-21 21:45:46 -0500

    Implement restaurant controller methods and pages.

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,21 +1,32 @@
 // index, create_get, create_post, details_get, edit_get, edit_post, delete
-// const Restaurant = require('../models/restaurant')
+const Restaurant = require('../models/restaurants');
 
 const restaurant_index = async (req, res) => {
-  //   const restaurants = await Restaurant.find({})
-  res.send('index');
+  const restaurants = await Restaurant.find({}).sort({ name: 1 });
+  console.log(restaurants);
+  res.render('restaurants/index', { restaurants });
 };
 
 const restaurant_create_get = async (req, res) => {
-  res.send('create get');
+  res.render('restaurants/create');
 };
 
 const restaurant_create_post = async (req, res) => {
-  res.send('create post');
+  const restaurant = new Restaurant(req.body.restaurant);
+  restaurant.author = req.user._id;
+  await restaurant.save();
+  console.log(restaurant);
+  // res.redirect(`/restaurants/${restaurant._id}`);
 };
 
 const restaurant_details = async (req, res) => {
-  res.send('details');
+  const { id } = req.params;
+  const restaurant = await Restaurant.findById(id);
+  if (!restaurant) {
+    req.flash('error', 'Not restaurant with given id.');
+    res.redirect('/restaurants');
+  }
+  res.render('restaurants/details', { restaurant });
 };
 
 const restaurant_edit_get = async (req, res) => {
@@ -27,7 +38,10 @@ const restaurant_edit_put = async (req, res) => {
 };
 
 const restaurant_delete = async (req, res) => {
-  res.send('delete');
+  const { id } = req.params;
+  await Restaurant.findByIdAndDelete(id);
+  req.flash('success', 'Restaurant deleted');
+  res.redirect('/restaurants');
 };
 
 module.exports = {
